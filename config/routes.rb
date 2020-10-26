8a53d5eaed8734f2444380c506939c65ab831978
Nicolas Emmery <nicolas.emmery@gmail.com>
2020-10-26 12:22:04 +0100

    i18n trad form inscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookings, only: [:new, :create]
-
-
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :bookings, only: [:new, :create]
+  end
 end
